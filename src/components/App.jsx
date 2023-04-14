a49c097d51e7c0be586a882c922559055b5df350
cmdspeed <cmdspeed@gmail.com>
2023-04-14 17:49:44 +0200

    task 03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
+import { FriendList } from './friendList/FriendList';
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
       </section>
       <Statistics title="Upload stats" stats={data} />;
       <Statistics stats={data} />
+      <FriendList friends={friends} />;
     </>
   );
 };
---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,14 @@
+// import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => (
+  <ul className={css.friendList}>
+    {friends.map(({ id, isOnline, avatar, name }) => (
+      <li className={css.item} key={id}>
+        <span className={isOnline ? css.online : css.offline}></span>
+        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={css.name}>{name}</p>
+      </li>
+    ))}
+  </ul>
+);
